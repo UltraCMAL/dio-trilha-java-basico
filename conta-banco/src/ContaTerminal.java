07702c2b5a998c40bf5b8b4230a721dc05822fdd
UltraCMAL <claudio8marcio@gmail.com>
2024-08-23 22:03:24 -0300

    Exercício ContaTerminal finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        try {
+            System.out.println("Por favor, digite seu nome!");
+            String nome=scanner.nextLine();
+            
+            System.out.println("Por favor, digite o número da Agência!");
+            String agencia=scanner.nextLine();
+
+            System.out.println("Por favor, digite o número da Conta!");
+            int conta=scanner.nextInt();
+
+            System.out.println("Por favor, digite o seu Saldo!");
+            double saldo=scanner.nextDouble();
+
+            System.out.println("Olá, " + nome + " obrigado por criar uma conta em nosso banco, sua agência é " 
+            + agencia + ", conta "+ conta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        } catch (Exception e) {
+            System.out.println("Erro, por favor, tente novamente.");
+        } finally {
+
+            scanner.close();
+    }
     }
 }
+
